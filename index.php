eaa201b9475793d71514c1e463d9bb7453ed64a5
Rajnai Bence <Rajnai Bence>
2021-03-22 14:41:39 +0100

    varosok

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <script src="feldolgoz.js" type="text/javascript"></script>
+        
+    </head>
+    <title>Vince Patrik</title>
+</head>
+<body>
+    <main>
+        <header>Varoslista</header>
+        <form action="feldolgoz.js" method="POST">
+            <label for="varos">Varos neve: </label>
+            <input type="text" id="varos" name="varos">
+        </form>
+
+        <article>
+
+        </article>
+    </main>
+
+</body>
+</html>
